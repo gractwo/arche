b78589eca78545e4f0c2df344a1765897aaefa6a
jakubmanczak <jakub@manczak.net>
2025-07-22 14:04:22 +0200

    implement /days/community (days since formation)

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,9 +1,11 @@
 use axum::{Router, http::StatusCode, routing::get};
+use chrono::{NaiveDate, Utc};
 
 pub fn init() -> Router {
     Router::new()
         .route("/", get(async || "root"))
         .route("/discord/member-count", get(get_member_count))
+        .route("/days/community", get(get_days_since_community_formation))
 }
 
 async fn get_member_count() -> (StatusCode, String) {
@@ -14,4 +16,12 @@ async fn get_member_count() -> (StatusCode, String) {
             format!("An error occured - could not fetch discord member count."),
         ),
     }
+}
+
+async fn get_days_since_community_formation() -> String {
+    let formation = NaiveDate::from_ymd_opt(2020, 6, 7).unwrap();
+    let today = Utc::now().date_naive();
+    let days = today.signed_duration_since(formation).num_days();
+
+    days.to_string()
 }
